9714825cb5d40c8b20b049548d7b1ca7c8998b3c
csonchen <cson_chensheng@163.com>
2020-05-23 23:22:35 +0800

    add: 编写菜单详情静态页

--- client/pages/menuDetail/menuDetail.js
+++ client/pages/menuDetail/menuDetail.js
@@ -1,6 +1,10 @@
 const { fetchMenuList } = require('../../services/menu');
 
 Page({
+  data: {
+    menuDesc: "<div style='color: red;'>测试下富文本</div>"
+  },
+
   onLoad() {
     fetchMenuList({
       foodType: '1'
